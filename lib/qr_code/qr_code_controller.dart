215a8a4e0820c8eb75ec7d6872c4e8b3d1b395cd
Rohith k <52594592+Rohithkard@users.noreply.github.com>
2025-01-06 22:28:13 +0530

    Update qr_code_controller.dart

--- lib/qr_code/qr_code_controller.dart
+++ lib/qr_code/qr_code_controller.dart
@@ -27,27 +27,27 @@ class QrCodeScannerController extends GetxController {
         Barcode? result = scanData;
         String qrCode = result.code ?? "";
 
-        String? guestId = qrCode.split("/").lastOrNull;
-        if (guestId?.length == 1) {
-          guestId = "UI A000$guestId";
-        } else if (guestId?.length == 2) {
-          guestId = "UI A00$guestId";
-        } else if (guestId?.length == 3) {
-          guestId = "UI A0$guestId";
-        } else if (guestId?.length == 4) {
-          guestId = "UI A$guestId";
-        } else {
-          controller.resumeCamera();
-          Get.showSnackbar(const GetSnackBar(
-              duration: Duration(seconds: 5),
-              backgroundColor: Colors.transparent,
-              padding: EdgeInsets.zero,
-              messageText: AppSnackBar(
-                text: "Invalid QR code",
-              )));
-        }
+        //String? guestId = qrCode.split("/").lastOrNull;
+        //if (guestId?.length == 1) {
+       //   guestId = "UI A000$guestId";
+       // } else if (guestId?.length == 2) {
+     //   guestId = "UI A00$guestId";
+       // } else if (guestId?.length == 3) {
+         // guestId = "UI A0$guestId";
+      //  } else if (guestId?.length == 4) {
+         // guestId = "UI A$guestId";
+        //} else {
+        //  controller.resumeCamera();
+         // Get.showSnackbar(const GetSnackBar(
+            //  duration: Duration(seconds: 5),
+              //backgroundColor: Colors.transparent,
+            //  padding: EdgeInsets.zero,
+            //  messageText: AppSnackBar(
+              //  text: "Invalid QR code",
+              //)));
+      //  }
 
-        getUserDetails(guestId);
+        getUserDetails(qrCode);
         controller.pauseCamera();
       } catch (error) {
         controller.resumeCamera();
